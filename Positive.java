162ed4c1d5f1a6a3764250cfbb49c47f772c707b
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-21 17:55:45 +0000

    Number

---
+++ Positive.java
@@ -0,0 +1,12 @@
+package Number;
+
+public class Positive {
+    public static void main(String[] args) {
+        int e = -14;
+        if (e>2) {
+            System.out.println("Negative");
+        }else{
+            System.out.println("Positive");
+        }
+    }
+}
---
+++ negative.java
@@ -0,0 +1,13 @@
+package Number;
+
+public class negative {
+    public static void main(String[] args) {
+        int c = -22;
+        if (c<0) {
+            System.out.println("Negative");
+        }else{
+            System.out.println("Positive");
+        }
+
+    }
+}
